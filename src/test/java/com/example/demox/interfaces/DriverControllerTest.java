c171edd3a1d86ecc8f56a7656da8fcb73cddc5e0
Piotr Ganicz <piotr.ganicz@gmail.com>
2017-08-10 16:39:20 +0200

    DriverController test

--- src/test/java/com/example/demox/interfaces/DriverControllerTest.java
+++ src/test/java/com/example/demox/interfaces/DriverControllerTest.java
@@ -1,4 +1,78 @@
 package com.example.demox.interfaces;
 
-public class DriverControllerTest {
+
+import com.example.demox.DemoxApplication;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Bean;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.UUID;
+
+import static org.springframework.test.util.AssertionErrors.assertEquals;
+import static org.springframework.test.util.AssertionErrors.assertTrue;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.setup.MockMvcBuilders.webAppContextSetup;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(classes = DemoxApplication.class)
+@WebAppConfiguration
+public class DriverControllerTest  {
+    private MockMvc mockMvc;
+    @Autowired
+    private WebApplicationContext webApplicationContext;
+
+    @Before
+    public void setup() throws Exception {
+        this.mockMvc = webAppContextSetup(webApplicationContext).build();
+    }
+
+    @Test
+    public void stopoverIdNotFound() throws Exception {
+        MvcResult result = mockMvc.perform(get("/stopover/fee")
+                .param("StopoverId","SDSFDF"))
+                .andDo(print())
+                .andExpect(status().isOk())
+                .andReturn();
+        String content = result.getResponse().getContentAsString();
+        assertEquals("It's a thief!", "false", content);
+    }
+
+    @Test
+    public void newStopover() throws Exception {
+        mockMvc.perform(post("/stopover/start")
+                .param("DriverId", "FDAFDASF"))
+                .andExpect(status().isOk());
+    }
+
+
+    @Test
+    public void newStartEnd() throws Exception {
+        MvcResult result = mockMvc.perform(post("/stopover/start")
+                .param("DriverId", "FDAFDASF"))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String uuidStr = result.getResponse().getContentAsString();
+        try {
+            UUID uuid = UUID.fromString(uuidStr);
+        } catch (IllegalArgumentException exception) {
+            assertTrue("The exception has been rised: " +exception.getMessage(),false);
+        }
+
+        MvcResult result2 = mockMvc.perform(post("/stopover/end")
+                .param("StopoverId", uuidStr))
+                .andExpect(status().isOk())
+                .andReturn();
+    }
 }
