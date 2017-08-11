81354966b131d5ae13e61bb6444476c71da1226a
Piotr Ganicz <piotr.ganicz@gmail.com>
2017-08-11 16:33:21 +0200

    Earning service refactor + test

--- src/main/java/com/example/demox/application/impl/EarningsServiceImpl.java
+++ src/main/java/com/example/demox/application/impl/EarningsServiceImpl.java
@@ -3,6 +3,7 @@ package com.example.demox.application.impl;
 import com.example.demox.application.EarningsService;
 import com.example.demox.domain.model.fee.Fee;
 import com.example.demox.domain.model.fee.FeeRepository;
+import jdk.nashorn.internal.objects.annotations.Function;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import java.math.BigDecimal;
@@ -21,9 +22,11 @@ public class EarningsServiceImpl implements EarningsService {
     @Override
     public Fee getTotalEarningsForDay(Date date) {
         Collection<Fee> collection = feeRepository.getFeesByDayOfYear(date);
-        long totalEarnings = collection.stream()
-                .mapToLong(fee -> fee.getFine().longValue())
-                .sum();
-        return new Fee(new BigDecimal(totalEarnings), "PLN");
+        BigDecimal total = collection
+                .stream()
+                .map(Fee::getFine)
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+
+        return new Fee(total, "PLN");
     }
 }
